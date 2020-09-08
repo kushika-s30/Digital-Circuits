77b4eebe923c1b5a1e44eafc32958f8cf2618da6
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-09-08 08:40:47 +0530

    Update HalfAdder.sv

--- Data Flow Modelling/HalfAdder.sv
+++ Data Flow Modelling/HalfAdder.sv
@@ -1,1 +1,26 @@
-Half adder 
+//Design.sv//
+module half_adder(a,b,s,c);
+  assign s=a^b;
+  assign c=a&b;
+endmodule
+
+//Testbench.sv//
+module half_adderuut;
+  reg a,b;
+  wire s,c;
+  half_adder uut(.a(a),
+                 .b(b),
+                 .s(s),
+                 .c(c));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    
+    a=0;b=0; #100
+    a=0;b=1; #100
+    a=1;b=0; #100
+    a=1;b=1; #100
+     
+    $finish; 
+  end 
+endmodule 
