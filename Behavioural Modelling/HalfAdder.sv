a97418dc596025a98a91830cd6adfec0c371db25
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-09-08 08:46:23 +0530

    Update HalfAdder.sv

--- Behavioural Modelling/HalfAdder.sv
+++ Behavioural Modelling/HalfAdder.sv
@@ -1,1 +1,29 @@
-Half Adder 
+//Design.sv//
+module half_adder(input a,b,output s,c);
+  reg s,c;
+  always@(a,b) begin
+    s=a^b;
+    c=a&b;
+  end  
+endmodule
+
+//Testbench.sv//
+module half_adderuut;
+  reg a,b;
+  wire s,c;
+  half_adder uut(.a(a),
+                 .b(b),
+                 .s(s),
+                 .c(c));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    
+    a=0;b=0; #100
+    a=0;b=1; #100
+    a=1;b=0; #100
+    a=1;b=1; #100
+     
+    $finish; 
+  end 
+endmodule 
