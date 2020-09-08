ff2a619fe84b92e55ede3f66593c06f33bb218d2
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-09-08 09:00:12 +0530

    Create HalfSubtarctor.sv

---
+++ Behavioural Modelling/HalfSubtarctor.sv
@@ -0,0 +1,30 @@
+//Design.sv//
+module half_sub(input a,b,output d,b1);
+  reg d,b1;
+  always@(a,b) begin
+    d=a^b;
+    b1=(~a)&b;
+  end  
+endmodule
+
+//Testbench.sv//
+module half_subuut;
+  reg a,b;
+  wire d,b1;
+  half_sub uut(.a(a),
+                 .b(b),
+                 .d(d),
+                 .b1(b1));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    
+    a=0;b=0; #100
+    a=0;b=1; #100
+    a=1;b=0; #100
+    a=1;b=1; #100
+     
+    $finish; 
+  end 
+endmodule 
+  
