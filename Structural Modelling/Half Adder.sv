b612bf645e56a54768a1604e831f5cb609c9f193
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-09-08 08:41:32 +0530

    Update Half Adder.sv

--- Structural Modelling/Half Adder.sv
+++ Structural Modelling/Half Adder.sv
@@ -1,1 +1,26 @@
-Half adder 
+//Design.sv//
+module half_adder(input a,b,output s,c);
+  xor(s,a,b);
+  and(c,a,b);
+endmodule
+
+//Testbench.sv//
+module half_adderuut;
+  reg a,b;
+  wire s,c;
+  half_adder uut(.a(a),
+                 .b(b),
+                 .s(s),
+                 .c(c));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    
+    a=0;b=0; #100
+    a=0;b=1; #100
+    a=1;b=0; #100
+    a=1;b=1; #100
+     
+    $finish; 
+  end 
+endmodule 
