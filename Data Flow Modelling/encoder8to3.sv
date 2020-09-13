4e2257cb744a765c78a3bdb88d5604c640cc7df8
Kushika Sivaprakasam <63924175+kushika-s30@users.noreply.github.com>
2020-09-13 16:13:30 +0530

    Update encoder8to3.sv

--- Data Flow Modelling/encoder8to3.sv
+++ Data Flow Modelling/encoder8to3.sv
@@ -1,17 +1,11 @@
 // Design (design.sv)//
 `timescale 1ns/1ps
-module encoder8x3(i,en,d);
+module encoder8x3(i,d);
   input [7:0]i;
-  input en;
   output [2:0]d;
-  if (en == 1) begin
   assign d[0]=(i[1])|(i[3])|(i[5])|(i[7]);
   assign d[1]=(i[2])|(i[3])|(i[6])|(i[7]);
   assign d[2]=(i[4])|(i[5])|(i[6])|(i[7]);
-  end
-  else begin
-    d=3'b000;
-  end
 endmodule
 
 
@@ -27,25 +21,14 @@ module encoder8x3tb;
     $dumpfile("dump.vcd");
     $dumpvars(1);
     
-    i=8'b00000001; en=0  #100
-    i=8'b00000010; en=0  #100
-    i=8'b00000100; en=0  #100
-    i=8'b00001000; en=0  #100
-    i=8'b00010000; en=0  #100
-    i=8'b00100000; en=0  #100
-    i=8'b01000000; en=0  #100
-    i=8'b10000000; en=0  #100
-    
-    i=8'b00000001; en=1  #100   
-    i=8'b00000010; en=1  #100
-    i=8'b00000100; en=1  #100
-    i=8'b00001000; en=1  #100
-    i=8'b00010000; en=1  #100
-    i=8'b00100000; en=1  #100
-    i=8'b01000000; en=1  #100
-    i=8'b10000000; en=1  #100
-    
-    
+    i=8'b00000001;   #100
+    i=8'b00000010;   #100
+    i=8'b00000100;   #100
+    i=8'b00001000;   #100
+    i=8'b00010000;   #100
+    i=8'b00100000;   #100
+    i=8'b01000000;   #100
+    i=8'b10000000;   #100
     
     $finish;  
   end  
